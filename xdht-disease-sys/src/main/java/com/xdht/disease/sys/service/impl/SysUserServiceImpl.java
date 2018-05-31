6e692eece6c2669db81a8665f0b37904357a728a
fenghao1 <1462439581@qq.com>
2018-05-31 10:55:14 +0800

    用户功能修改

--- xdht-disease-sys/src/main/java/com/xdht/disease/sys/service/impl/SysUserServiceImpl.java
+++ xdht-disease-sys/src/main/java/com/xdht/disease/sys/service/impl/SysUserServiceImpl.java
@@ -1,73 +1,23 @@
 package com.xdht.disease.sys.service.impl;
 
-import com.xdht.disease.common.authorization.manager.TokenManager;
-import com.xdht.disease.common.core.AbstractService;
-import com.xdht.disease.common.model.TokenModel;
-import com.xdht.disease.common.model.User;
 import com.xdht.disease.sys.dao.SysUserMapper;
 import com.xdht.disease.sys.model.SysUser;
 import com.xdht.disease.sys.service.SysUserService;
-import com.xdht.disease.sys.vo.request.LoginRequest;
-import com.xdht.disease.sys.vo.request.SysUserRequest;
-import com.xdht.disease.sys.vo.response.LoginResponse;
-import com.xdht.disease.sys.vo.response.SysUserResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.xdht.disease.core.AbstractService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import tk.mybatis.mapper.entity.Condition;
 
 import javax.annotation.Resource;
-import java.util.LinkedList;
-import java.util.List;
 
 
 /**
- * Created by lzf on 2018/05/29.
+ * Created by lzf on 2018/05/31.
  */
 @Service
 @Transactional
-public class SysUserServiceImpl extends AbstractService<SysUser> implements SysUserService {
+public class SysUserServiceImpl extends AbstractService<SysUser> implements SysUserService
 
     @Autowired
     private SysUserMapper sysUserMapper;
-
-    @Resource(name = "ehcacheTokenManager")
-    private TokenManager tokenManager;
-
-    @Override
-    public LoginResponse createToken(LoginRequest loginRequest) {
-        LoginResponse response = new LoginResponse();
-
-        SysUser sysUser = new SysUser();
-        sysUser.setId(loginRequest.getUserId());
-
-        sysUser = sysUserMapper.selectByPrimaryKey(sysUser);
-        User user = new User();
-        user.setId(sysUser.getId());
-        user.setName(sysUser.getUserName());
-
-        //创建token
-        TokenModel tokenModel = tokenManager.createToken(user);
-        response.setToken(tokenModel.getToken());
-        return response;
-    }
-
-    @Override
-    public List<SysUserResponse> querySysUserList(SysUserRequest sysUserRequest) {
-        List<SysUserResponse> sysUserResponseList = new LinkedList<>();
-        Condition condition = new Condition(SysUser.class);
-        condition.createCriteria().andLike("userName", "%" + sysUserRequest.getUserName() + "%");
-        List<SysUser> sysUserList = this.sysUserMapper.selectByCondition(condition);
-        if (sysUserList != null && sysUserList.size() > 0) {
-            for (SysUser sysUser : sysUserList
-                 ) {
-                SysUserResponse sysUserResponse = new SysUserResponse();
-                sysUserResponse.setId(sysUser.getId());
-                sysUserResponse.setUserName(sysUser.getUserName());
-                sysUserResponseList.add(sysUserResponse);
-            }
-        }
-        return sysUserResponseList;
-    }
 
 }
